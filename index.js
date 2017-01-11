4d330bef2920a99a5608db23696491a571e14ba4
ubuntu <hburt11@gmail.com>
2017-01-10 21:34:26 -0800

    refactor fileserver

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 
+
 http.createServer(function(req, res) {
 
   console.log(req.method, req.url);
 
+  var filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
   var contentType = 'text/html';
+  var extname = path.extname(filePath);
   switch (extname) {
     case '.js':
       contentType = 'text/javascript';
@@ -20,18 +27,23 @@ http.createServer(function(req, res) {
       break;
   }
 
-  if (req.url === '/') {
-    fs.readFile('index.html', 'UTF-8', function (err, content) {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    })
+//  if (req.url === '/') {
+  fs.readFile(filePath, 'UTF-8', function (err, content) {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 File Not Found');
+      } else {
+        throw err;
+      }
+    } else {
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(content, 'utf-8');
+    }
+  });
 
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('404 File Not Found');
-  }
 
 }).listen(process.env.PORT || 3000);
 
 
-console.log('hello');
+console.log('Server started');
